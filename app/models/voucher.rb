968f032e13bb6d10d4f0c9c134dff68b8d022d9f
Ajay Kumar G <ajay@aapamtech.com>
2009-05-25 19:47:08 -0700

    Removed stuff for voucher abt to introduce subscription system

--- app/models/voucher.rb
+++
@@ -1,4 +0,0 @@
-class Voucher < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :plan
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,9 +21,14 @@ end
 
 config.after_initialize do
   ActiveMerchant::Billing::Base.mode = :test
-  ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
-    :login => "seller_1229899173_biz_api1.railscasts.com",
-    :password => "FXWU58S7KXFC6HBE",
-    :signature => "AGjv6SW.mTiKxtkm6L9DcSUCUgePAUDQ3L-kTdszkPG8mRfjaRZDYtSu"
-  )
+#  ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
+#    :login => "demo",
+#    :password => "password",
+#  )
+  gateway = ActiveMerchant::Billing::BraintreeGateway.new({ 
+    :login    => 'demo', 
+    :password => 'password'
+  })
 end
+
+
